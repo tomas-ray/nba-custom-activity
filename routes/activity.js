1896e65345fc746ae53b87fcd04c78b35486e7c9
juan.francia@prudential.com.sg <juan.francia@prudential.com.sg>
2019-10-25 12:37:48 +0800

    update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -50,10 +50,10 @@ exports.edit = function (req, res) {
  */
 exports.save = function (req, res) {
     console.log('Save');
-    console.log('req - >' + req);
-    console.log('req - > ' + jsonCircular.stringify(req));
-    console.log('req.body - > ' + req.data);
-    console.log('req.body - > ' + jsonCircular.stringify(req.data));
+    // console.log('req - >' + req);
+    // console.log('req - > ' + jsonCircular.stringify(req));
+    // console.log('req.body - > ' + req.data);
+    // console.log('req.body - > ' + jsonCircular.stringify(req.data));
     res.send(200, 'Save');
 };
 
@@ -63,10 +63,10 @@ exports.save = function (req, res) {
 exports.publish = function (req, res) {
     logData(req);
     console.log('Publish');
-    console.log('req - >' + req);
-    console.log('req - > ' + jsonCircular.stringify(req));
-    console.log('req.body - > ' + req.data);
-    console.log('req.body - > ' + jsonCircular.stringify(req.data));
+    // console.log('req - >' + req);
+    // console.log('req - > ' + jsonCircular.stringify(req));
+    // console.log('req.body - > ' + req.data);
+    // console.log('req.body - > ' + jsonCircular.stringify(req.data));
     res.send(200, 'Publish');
 };
 
@@ -76,10 +76,10 @@ exports.publish = function (req, res) {
 exports.validate = function (req, res) {
     logData(req);
     console.log('Validate');
-    console.log('req - >' + req);
-    console.log('req - > ' + jsonCircular.stringify(req));
-    console.log('req.body - > ' + req.data);
-    console.log('req.body - > ' + jsonCircular.stringify(req.data));
+    // console.log('req - >' + req);
+    // console.log('req - > ' + jsonCircular.stringify(req));
+    // console.log('req.body - > ' + req.data);
+    // console.log('req.body - > ' + jsonCircular.stringify(req.data));
     res.send(200, 'Validate');
     console.log("end validate function!!!");
 };
@@ -89,10 +89,10 @@ exports.validate = function (req, res) {
  */
 exports.stop = function (req, res) {
     console.log('Stop');
-    console.log('req - >' + req);
-    console.log('req - > ' + jsonCircular.stringify(req));
-    console.log('req.body - > ' + req.data);
-    console.log('req.body - > ' + jsonCircular.stringify(req.data));
+    // console.log('req - >' + req);
+    // console.log('req - > ' + jsonCircular.stringify(req));
+    // console.log('req.body - > ' + req.data);
+    // console.log('req.body - > ' + jsonCircular.stringify(req.data));
     logData(req);
     res.send(200, 'Stop');
 };
@@ -122,22 +122,21 @@ exports.execute = function (req, res) {
         var jsonObject = JSON.parse(body);
         var token = jsonObject["access_token"];
         //getDataXMLJD(decodedArgs,token);
+        console.log('TOKEN - > ' + token);
+        console.log('REQ BODY - > ' + req.body);
+        
         JWT(token, process.env.JWT_KEY, (err, decoded) => {
             let responseBody = '';
             if (err) {
                 console.error(err);
                 return res.status(401).end();
             }
-    
-    
             if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
                 console.log('##### decoded ####=>', decoded);
                 var decodedArgs = decoded.inArguments[0];
                 //getMcTokenJD(decodedArgs);
                 getDataXMLJD(decodedArgs,token);
-    
                 return res.send(200, 'Execute');
-            
             } else {
                 console.error('inArguments invalid.');
                 return res.status(400).end();
